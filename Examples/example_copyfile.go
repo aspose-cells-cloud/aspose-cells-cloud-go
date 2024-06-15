fa3a2b247527c4d5f38c58a9fceddd1c30a70031
roy.wang <roy.wang@aspose.com>
2024-06-15 17:43:02 +0800

    2024-06-15 : Synchronize codes to the origin repository.

--- Examples/example_copyfile.go
+++ Examples/example_copyfile.go
@@ -5,31 +5,31 @@ import (
 
 	asposecellscloud "github.com/aspose-cells-cloud/aspose-cells-cloud-go"
 )
+
 func main() {
 	instance := asposecellscloud.NewCellsApiService(os.Getenv("ProductClientId"), os.Getenv("ProductClientSecret"), "https://api.aspose.cloud", "v3.0")
-    remoteFolder := "TestData/In"
+	remoteFolder := "TestData/In"
 
-    localName := "Book1.xlsx"
-    remoteName := "Book1.xlsx"
+	localName := "Book1.xlsx"
+	remoteName := "Book1.xlsx"
 
-    localNameRequest := new(asposecellscloud.UploadFileRequest)
-    localNameRequest.UploadFiles = make(map[string]string) 
-    localNameRequest.UploadFiles[localName] =   localName
-    localNameRequest.Path = remoteFolder + "/" + remoteName 
-    localNameRequest.StorageName =""
-    instance.UploadFile(localNameRequest )
- 
+	localNameRequest := new(asposecellscloud.UploadFileRequest)
+	localNameRequest.UploadFiles = make(map[string]string)
+	localNameRequest.UploadFiles[localName] = localName
+	localNameRequest.Path = remoteFolder + "/" + remoteName
+	localNameRequest.StorageName = ""
+	instance.UploadFile(localNameRequest)
 
-    request := new (asposecellscloud.CopyFileRequest)
-    request.SrcPath =         remoteFolder + "/" + remoteName    
-    request.DestPath =         "OutResult/" + remoteName    
-    request.SrcStorageName =         ""    
-    request.DestStorageName =         ""    
-    request.VersionId =         ""    
-    httpResponse, err := instance.CopyFile(request)
+	request := new(asposecellscloud.CopyFileRequest)
+	request.SrcPath = remoteFolder + "/" + remoteName
+	request.DestPath = "OutResult/" + remoteName
+	request.SrcStorageName = ""
+	request.DestStorageName = ""
+	request.VersionId = ""
+	httpResponse, err := instance.CopyFile(request)
 	if err != nil {
-		t.Error(err)
+		print(err)
 	} else if httpResponse.StatusCode < 200 || httpResponse.StatusCode > 299 {
-		t.Fail()
+		print("Test fail")
 	}
 }
