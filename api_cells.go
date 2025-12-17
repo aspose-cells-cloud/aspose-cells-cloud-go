a3c1fdd7894e68623f8d2205b9af35c84b2e4314
roy.wang <roy.wang@aspose.com>
2025-12-17 21:24:06 +0800

    ISSUE-CELLSCLOUD-13605: Build Aspose.Cells Cloud SDK 25.12.

--- api_cells.go
+++ api_cells.go
@@ -39,7 +39,7 @@ type CellsCloudOption struct {
 }
 
 func Version() {
-	fmt.Println("---Version: 25.11---")
+	fmt.Println("---Version: 25.12---")
 }
 
 func NewCellsApiService(appSid string, appKey string, opts ...string) *CellsApiService {
@@ -1480,6 +1480,50 @@ func (a *CellsApiService) MergeRemoteSpreadsheet(data *MergeRemoteSpreadsheetReq
 
 
 func (a *CellsApiService) SplitSpreadsheet(data *SplitSpreadsheetRequest   , options ...CellsCloudOption) (  []byte,  *http.Response, error) {
+	var (
+	localVarReturnValue []byte 
+
+	)
+
+    r, err := data.CreateRequestData(a.client);
+    if err != nil {
+        return  localVarReturnValue,  nil, err
+    }
+
+	localVarHttpResponse, err := a.client.callAPI(r)
+	if err != nil || localVarHttpResponse == nil {
+		return localVarReturnValue,  localVarHttpResponse, err
+	}
+	defer localVarHttpResponse.Body.Close()
+	if localVarHttpResponse.StatusCode >= 300 {
+		bodyBytes, _ := ioutil.ReadAll(localVarHttpResponse.Body)
+		return localVarReturnValue,   localVarHttpResponse, reportError("Status: %v, Body: %s", localVarHttpResponse.Status, bodyBytes)
+	}
+	 
+		if localVarReturnValue, err = ioutil.ReadAll(localVarHttpResponse.Body); err != nil {
+			return localVarReturnValue, localVarHttpResponse, err
+		}
+ 
+	if len(options) > 0 {
+		for _, option := range options {
+			if strings.EqualFold(option.OptionName, "LocalOutPath") {
+				file, err := os.OpenFile(option.OptionValue, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+				if err != nil {
+					panic(err)
+				}
+				defer file.Close()
+
+				if _, err := file.Write(localVarReturnValue); err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+	return localVarReturnValue,  localVarHttpResponse, err
+}
+
+
+func (a *CellsApiService) SplitTable(data *SplitTableRequest   , options ...CellsCloudOption) (  []byte,  *http.Response, error) {
 	var (
 	localVarReturnValue []byte 
 
